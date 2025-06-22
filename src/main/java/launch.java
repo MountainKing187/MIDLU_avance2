71688b3fe3da0c71862ec70f351222648065f961
MountainKing187 <g.vergara05@ufromail.cl>
2025-06-21 21:51:21 -0400

    Ordenado launch y añadido comentarios

--- src/main/java/launch.java
+++ src/main/java/launch.java
@@ -1,4 +1,3 @@
-import modelo.PuntoAcceso;
 import modelo.edificio.Edificio;
 import persistencia.CargadorEdificios;
 import servicios.Navegador;
@@ -11,28 +10,29 @@ import java.util.ArrayList;
 public class launch {
     public static void main(String[] args) throws Exception {
 
-        Edificio r = CargadorEdificios.cargarDesdeJSON("src/main/resources/EdificiosJSON/EdificioEjemplo.json");
-        Navegador navegador = new Navegador(r);
+        Edificio r = CargadorEdificios.cargarDesdeJSON("src/main/resources/EdificiosJSON/EdificioEjemplo.json"); //Crea un edificio desde un json
+        Navegador navegador = new Navegador(r); //Crea un objeto navegador usando el edificio recien creado
+
+        //Puntos que se usaran para navegar
         Punto punto1 = new Punto(53,50,r.getPiso(1));
         Punto punto2 = new Punto(53,12,r.getPiso(2));
+
         ArrayList<Ruta> rutas = navegador.calcularRutaCompleta(punto1,punto2,false);
+        //Se crea un array de rutas, cada ruta ocurre en un solo piso, en este ejemplo, el primer elemento de rutas,
+        //es la ruta del primer punto hacia un punto de acceso mas cercano que este disponible
 
-        System.out.println(rutas.size());
-
+        //este codigo se hace para imprimir cada ruta por piso
         for (Ruta ruta : rutas){
             for (Punto punto : ruta.getPuntos()){
-                System.out.println("(x= "+punto.getX()+", y= "+punto.getY()+", piso="+punto.getPiso().getNombre()+")");
+                // System.out.println("(x= "+punto.getX()+", y= "+punto.getY()+", piso="+punto.getPiso().getNombre()+")");
+                //esta linea imprime las coordenadas de todos los puntos dentro de la ruta y el piso que estan localizadas
             }
+            imprimirRuta(ruta.getPuntoInicio().getPiso(), ruta);
         }
-        imprimirRuta(punto1.getPiso(),rutas.get(0));
-        imprimirRuta(punto2.getPiso(),rutas.get(1));
-        /*
-        imprimirRuta(r.getPiso(1),rutas.getFirst());
-        imprimirRuta(r.getPiso(2),rutas.get(2));
-        */
 
     }
-
+    // Metodo para imprimir una matriz con las posiciones de las paredes y rutas dentro
+    // no imprime puntos de accesos
     private static void imprimirRuta(Piso piso, Ruta ruta){
         boolean dibuja = false;
         boolean[][] obstaculos = piso.getMapaObstaculos();
